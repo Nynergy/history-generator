92d10e32dc9d3a9613c9360236331abc7edc7456
Ben Buchanan <bsbuchanan99@gmail.com>
2018-11-16 16:10:26 -0500

    Added main.py file placeholder

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+    History Generator by Ben Buchanan (2018)
+'''
+def GenerateName():
+    print("This is the function that will generate a random name.")
+    print("Generating name...")
+    name = "Name generated!"
+
+    return name
+
+def main():
+    print("This is the main file of the project.")
+    name = GenerateName()
+    print(name)
+
+    return
+
+main()
