6b16b0a343252b2e89158522f0dc5f7d5d4fbb8a
Ben Buchanan <bsbuchanan99@gmail.com>
2018-11-17 21:19:21 -0500

    More events handled

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from classes.Event import Event
     Global lists to pull from
 '''
 
-generic_nouns = ["apple", "trumpet", "book", "rose", "heron", "vileness", "glitter", "space", "rock", "face", "pole", "solitude", "menagerie", "zoetrope", "wizard", "joke", "fern", "cloud", "moon", "sun", "weed", "lock", "bottle", "crown", "key", "mouse", "window", "snow", "candle", "throne", "sceptre", "trial", "illness", "hospitality", "goodwill", "tomato", "rice"]
+generic_nouns = ["apple", "trumpet", "book", "rose", "heron", "volume", "glitter", "space", "rock", "face", "pole", "solitude", "menagerie", "zoetrope", "wizard", "joke", "fern", "cloud", "moon", "sun", "weed", "lock", "bottle", "crown", "key", "moth", "window", "snow", "candle", "throne", "sceptre", "trial", "pathogen", "friend", "will", "leek", "rice"]
 adjective_list = ["dirty", "dusty", "shady", "rainy", "red", "blue", "green", "clean", "smelly", "polite", "lovely", "bloody", "dilapidated", "ancient", "modern", "futuristic", "crazy", "quiet", "loud", "holy", "evil", "high", "low", "crusty", "smooth", "rough", "quick", "wild", "golden", "black", "gloomy", "sunny", "broken", "sunken", "raised", "gilded", "embossed", "yellow", "orange", "purple", "tired", "sick", "pleasant", "taut", "viscous"]
 
 material_list = ["stone", "limestone", "onyx", "amber", "diamond", "wood", "emerald", "ruby", "sapphire", "opal", "obsidian", "glass", "fabric of time", "topaz", "peridot", "steel", "iron", "chalk", "talc", "graphite", "plastic", "mica", "shale", "basalt", "pumice", "ice", "sandstone", "ash", "mud", "lead", "copper", "titanium", "uranium", "fabric of space", "solid light", "dust", "coal", "stained glass", "iridescent plates", "bone"]
@@ -44,9 +44,9 @@ def ResolveRelicSubtype(event, figure):
     description = ""
 
     if(event.event_subtype == "good relic"):
-        description = "{} found a relic of the {} civilization, which created a bright radiance around itself. {} called it the {} of {}.".format(figure.name, GenerateName(), figure.name, random.choice(random.choice([tools_list, weapons_list])).title(), random.choice(generic_nouns).title())
+        description = "{} found a relic of the {} civilization, which created a bright radiance around itself. {} called it the {} of {}s.".format(figure.name, GenerateName(), figure.name, random.choice(random.choice([tools_list, weapons_list])).title(), random.choice(generic_nouns).title())
     elif(event.event_subtype == "bad relic"):
-        description = "{} found a relic of the {} civilization, surrounded by an evil presence. Frightened, {} cast it away, declaring it {}, {} of {}.".format(figure.name, GenerateName(), figure.name, GenerateName(), random.choice(random.choice([tools_list, weapons_list])).title(), random.choice(generic_nouns).title())
+        description = "{} found a relic of the {} civilization, surrounded by an evil presence. Frightened, {} cast it away, declaring it {}, {} of {}s.".format(figure.name, GenerateName(), figure.name, GenerateName(), random.choice(random.choice([tools_list, weapons_list])).title(), random.choice(generic_nouns).title())
     elif(event.event_subtype == "normal"):
         civ_name = GenerateName()
         material_one = random.choice(material_list)
@@ -54,6 +54,213 @@ def ResolveRelicSubtype(event, figure):
         while(material_one == material_two):
             material_two = random.choice(material_list)
         description = "{} found a relic of {}, carved in {} and {}, and named it the {} of {}.".format(figure.name, civ_name, material_one, material_two, random.choice(generic_nouns).title(), civ_name)
+
+    return description
+
+# Resolves subtypes for seigeing a city
+def ResolveSeigeSubtype(event, figure):
+    description = ""
+
+    if(event.event_subtype == "to expand territory"):
+        enemy_fac = random.choice(figure.enemy_factions)
+        description = "In order to expand the territory of their domain, {} led a seige upon the city of {}, which was under control of the {}. {} fought with such ferocity that they were given the title {}, {} of {}.".format(figure.name, GenerateLocationName(), enemy_fac, figure.name, figure.name, random.choice(["Killer", "Flayer", "Subjugator", "Scourge", "Enemy", "Murderer", "Demon", "Crusher", "Executioner"]), enemy_fac.title())
+    elif(event.event_subtype == "to liberate a faction"):
+        friendly_fac = random.choice(figure.allied_factions)
+        description = "In order to liberate the {} from the city at {}, {} led a seige against it, earning them the title of {}, {} of {}.".format(friendly_fac, GenerateLocationName(), figure.name, figure.name, random.choice(["Friend", "Savior", "Angel", "Helper", "Blessing", "Boon", "Honor", "Embodiment", "Vision"]), friendly_fac.title())
+    elif(event.event_subtype == "to retrieve an artifact"):
+        noun_one = random.choice(generic_nouns)
+        noun_two = random.choice(generic_nouns)
+        while(noun_one == noun_two):
+            noun_two = random.choice(generic_nouns)
+        description = "{} led a seige against the city at {} in order to retrieve the fabled artifact {} of {}s and {}s.".format(figure.name, GenerateLocationName(), GenerateName(), noun_one.title(), noun_two.title())
+    elif(event.event_subtype == "normal"):
+        description = "Seemingly on a whim, {} decided to raze the city at {}, decimating the population of {} that lived there.".format(figure.name, GenerateLocationName(), random.choice(figure.enemy_factions))
+
+    return description
+
+# Resolves subtypes for finding ancient secrets
+def ResolveAncientSecretSubtype(event, figure):
+    description = ""
+
+    if(event.event_subtype == "awakens a god"):
+        description = "When delving some ancient cave systems for an expedition around {}, {} stumbled upon some strange runes. When spoken aloud, a terrifying vision filled their head of an ancient {} older than time itself, slowly waking from an infinite slumber. The only words that came to mind were frenzied screams of their name, {}'{}.".format(GenerateLocationName(), figure.name, random.choice(["god", "goddess", "deity", "demigod", "horror", "sentient force", "omniscience"]), GenerateName(), GenerateName().lower())
+    elif(event.event_subtype == "releases a curse"):
+        description = "After discovering an ancient tome of knowledge in a forgotten cave system around {}, {} found that a curse had been released upon the kingdom! In order to reverse the damage, the tome, The Index of {}s, was thrown into the fires of {}.".format(GenerateLocationName(), figure.name, random.choice(generic_nouns).title(), GenerateLocationName())
+    elif(event.event_subtype == "reveals a civilization"):
+        description = "Deep in the belly of the earth, {} discovered the remains of an ancient civilization called {}. Among the treasures kept hidden there was the fabled weapon, The {} {} of {}. {} brought it back as a prize of their discovery.".format(figure.name, GenerateName(), random.choice(adjective_list).title(), random.choice(weapons_list).title(), GenerateName(), figure.name)
+    elif(event.event_subtype == "gains magical powers"):
+        description = "With the incantation of ancient runes on the wall of a cave, {} summons a forgotten magical energy into their body, infusing them with the power to {}!".format(figure.name, random.choice(["spit acid", "shoot fire from their hands", "freeze anything by looking at it", "commune with animals", "read a person's mind", "see through walls", "fly", "glow in the dark", "persuade anybody", "disintegrate anything with a single thought", "shoot lightning from their fingertips", "move at the speed of light", "manipulate objects with telekinesis"]))
+    elif(event.event_subtype == "normal"):
+        description = "{} was able to locate the lost treasures of {} in the Mines of {}, earning them limitless riches!".format(figure.name, GenerateName(), GenerateLocationName())
+
+    return description
+
+# Resolves subtypes for assassinating a rival
+def ResolveAssassinationSubtype(event, figure):
+    description = ""
+
+    if(event.event_subtype == "normal"):
+        description = "In a fit of revenge against the murder of their {}, {} ordered the assassination of their rival {} of the clan of {}.".format(random.choice(["mother", "father", "brother", "sister", "uncle", "aunt", "grandfather", "grandmother", "son", "daughter", "niece", "nephew", "cousin"]), figure.name, GenerateName(), random.choice(figure.enemy_factions))
+
+    return description
+
+# Resolves subtypes for building a monument
+def ResolveMonumentSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for founding a religion
+def ResolveReligionSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for betraying a partner
+def ResolveBetrayalSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for marriage
+def ResolveMarriageSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for battling for a faction
+def ResolveFactionBattleSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for having a vision
+def ResolveVisionSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for commissioning massive weapons
+def ResolveMassiveWeaponSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for losing an artifact
+def ResolveArtifactLossSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for calling crusades
+def ResolveCrusadeSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for decreeing important laws
+def ResolveDecreeSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for having a child
+def ResolveChildSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for victimizing a faction
+def ResolveVictimizationSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for winning a major war
+def ResolveWarSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for inventing something important
+def ResolveInventionSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for gathering favor with a faction
+def ResolveFavorSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for building a temple
+def ResolveTempleSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for creating a piece of art
+def ResolveArtSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for holding a major festival
+def ResolveFestivalSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for falling under a curse
+def ResolveCurseSubtype(event, figure):
+    description = ""
+
+    # Handlers
+
+    return description
+
+# Resolves subtypes for becoming obsessed with something
+def ResolveObsessionSubtype(event, figure):
+    description = ""
+
+    # Handlers
 
     return description
 
@@ -87,9 +294,9 @@ def ResolveEvent(event, figure):
     event_type_handler = {
                             "birth":ResolveBirthSubtype(event, figure),
                             "discovers a relic":ResolveRelicSubtype(event, figure),
-                            #"seiges a city":ResolveSeigeSubtype(event, figure),
-                            #"unearths an ancient secret":ResolveAncientSecretSubtype(event, figure),
-                            #"assassinates a rival":ResolveAssassinationSubtype(event, figure),
+                            "seiges a city":ResolveSeigeSubtype(event, figure),
+                            "unearths an ancient secret":ResolveAncientSecretSubtype(event, figure),
+                            "assassinates a rival":ResolveAssassinationSubtype(event, figure),
                             #"builds a monument":ResolveMonumentSubtype(event, figure),
                             #"founds a religion":ResolveReligionSubtype(event, figure),
                             #"betrays a partner":ResolveBetrayalSubtype(event, figure),
@@ -179,7 +386,7 @@ def main():
     GenerateLifetime(figure)
     print("EVENTS:")
     for i in range(len(figure.events)):
-        print("Event {}: {} ({})\n\n{}\n".format(i+1, figure.events[i].event_type.title(), figure.events[i].event_subtype.title(), figure.events[i].description))
+        print("\tEvent {}: {} ({})\n\n{}\n".format(i+1, figure.events[i].event_type.title(), figure.events[i].event_subtype.title(), figure.events[i].description))
 
     return
 
